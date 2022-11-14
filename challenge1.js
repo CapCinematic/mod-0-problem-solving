3fc1069af278ecb75f5be1368581ed57ceb5e969
Isaiah Richardson <capcinematic@gmail.com>
2022-11-14 12:19:50 -0700

    Challenge1

--- challenge1.js
+++ challenge1.js
@@ -1,1 +1,8 @@
-var friends = ["tom", "jerry", "hugh", "jess", "sarah"]
+// Given an array of strings, return only the strings that have exactly 4 characters.
+
+var friends = ["tom", "jerry", "hugh", "jess", "sara"]
+for (var i = 0; i < friends.length; i++);{
+    var friend = friends[i]
+    if (friend.length === 4){
+    console.log(friends[i])}
+}
